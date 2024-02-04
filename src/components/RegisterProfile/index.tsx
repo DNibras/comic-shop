9c7775f7eba50a0b43c04a1086a667fc6375e86b
DNibras <demonnibras@gmail.com>
2024-02-04 23:09:17 +0300

    add style for RegisterProfile

--- src/components/RegisterProfile/index.tsx
+++ src/components/RegisterProfile/index.tsx
@@ -1,40 +1,44 @@
+import './style.css'
+
 const RegisterProfile = () => {
 
   return (
     <form className="register-profile-container">
       <h1>Регистрация профиля</h1>
-      <div>
-        <label htmlFor="email">Эл. почта *</label>
+      <div className="email-container">
+        <label htmlFor="email">Эл. почта*</label>
         <input
           type="email"
           name="email"
           id="email"
+          placeholder="Почта"
         />
       </div>
-      <div>
-        <label htmlFor="password">'Пароль *</label>
+      <div className="password-container">
+        <label htmlFor="password">Пароль*</label>
         <input
           type="password"
           name="password"
           id="password"
+          placeholder="Пароль"
         />
         <input 
           type="password" 
           id="confirmPassword" 
-          placeholder="Confirm Password"
+          placeholder="Подтвердите пароль"
           className="confirm-password-input"
         />
       </div>
-      <div>
-        <label htmlFor="fullName">ФИО: *</label>
+      <div className="fullName-container">
+        <label htmlFor="fullName">ФИО*</label>
         <input type="text" id="fullName" placeholder="ФИО" />
       </div>
-      <div>
-        <label htmlFor="phone">Телефон: *</label>
+      <div className="phone-container">
+        <label htmlFor="phone">Телефон*</label>
         <input type="tel" id="phone" placeholder="Телефон" />
       </div>
-      <div>
-        <label htmlFor="birthday">Дата рождения:</label>
+      <div className="birthday-container">
+        <label htmlFor="birthday">Дата рождения</label>
         <input type="date" id="birthday" />
       </div>
       <button className="register-button" type="submit">Зарегистрироваться</button>
