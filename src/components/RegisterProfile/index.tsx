3c8d8f4f0de20366b565d59f4eacd832a3f50aa4
DNibras <demonnibras@gmail.com>
2024-02-04 23:40:19 +0300

    corrected RegisterProfile

--- src/components/RegisterProfile/index.tsx
+++ src/components/RegisterProfile/index.tsx
@@ -3,46 +3,73 @@ import './style.css'
 const RegisterProfile = () => {
 
   return (
-    <form className="register-profile-container">
-      <h1>Регистрация профиля</h1>
-      <div className="email-container">
-        <label htmlFor="email">Эл. почта*</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Почта"
-        />
-      </div>
-      <div className="password-container">
-        <label htmlFor="password">Пароль*</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Пароль"
-        />
-        <input 
-          type="password" 
-          id="confirmPassword" 
-          placeholder="Подтвердите пароль"
-          className="confirm-password-input"
-        />
-      </div>
-      <div className="fullName-container">
-        <label htmlFor="fullName">ФИО*</label>
-        <input type="text" id="fullName" placeholder="ФИО" />
-      </div>
-      <div className="phone-container">
-        <label htmlFor="phone">Телефон*</label>
-        <input type="tel" id="phone" placeholder="Телефон" />
-      </div>
-      <div className="birthday-container">
-        <label htmlFor="birthday">Дата рождения</label>
-        <input type="date" id="birthday" />
-      </div>
-      <button className="register-button" type="submit">Зарегистрироваться</button>
-    </form>
+    <div className="register-profile-container">
+      <form className="register-profile-form">
+        <h1>Регистрация профиля</h1>
+        <div className="email-container">
+          <label htmlFor="email">Эл. почта*</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Почта"
+            className='register-input'
+          />
+        </div>
+        <div className="password-container">
+          <label htmlFor="password">Пароль*</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Пароль"
+            className='register-input'
+          />
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="Подтвердите пароль"
+            className='register-input'
+          />
+        </div>
+        <div className="fullName-container">
+          <label htmlFor="fullName">ФИО*</label>
+          <input
+            type="text" 
+            id="fullName" 
+            placeholder="ФИО" 
+            className='register-input'
+          />
+        </div>
+        <div className="phone-container">
+          <label htmlFor="phone">Телефон*</label>
+          <input 
+            type="tel" 
+            id="phone" 
+            placeholder="Телефон" 
+            className='register-input'
+          />
+        </div>
+        <div className="birthday-container">
+          <label htmlFor="birthday">Дата рождения</label>
+          <input 
+            type="date" 
+            id="birthday" 
+            className='register-input'
+          />
+        </div>
+        <div className="agree-container">
+          <input
+            type="checkbox" 
+            id="agree" 
+            className="agree-input"
+          />
+          <label htmlFor="agree">Я согласен с условиями пользовательского соглашения</label>
+        </div>
+        <button className="register-button" type="submit">Зарегистрироваться</button>
+      </form>
+      <p>* - поля, обязательные к заполнению</p>
+    </div>
   )
 }
 
